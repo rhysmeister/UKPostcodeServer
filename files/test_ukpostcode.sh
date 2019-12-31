19e5dd3ce594674c73b43a1d77edfcb64a7e3118
Rhys Campbell <rhys.james.campbell@googlemail.com>
2019-12-31 12:30:51 +0100

    Update test script

--- files/test_ukpostcode.sh
+++ files/test_ukpostcode.sh
@@ -3,7 +3,8 @@
 set -u;
 set -x;
 
-EXPECTED='[{"hostname":"9c14cd1e7d5a","id":316821,"latitude":"-0.759072483676255","longitude":"51.577854590183300","postcode":"SL7 1UQ"}]'
-RESULT=$(curl -s http://localhost:8080?postcode=sl7%201uq);
+EXPECTED='"id":316821,"latitude":"-0.759072483676255","longitude":"51.577854590183300","postcode":"SL7 1UQ"}]'
+# Trim off hostname because that is random
+RESULT=$(curl -s http://localhost:8080?postcode=sl7%201uq | cut -d "," -f2-5);
 
 [[ "$RESULT" == "$EXPECTED" ]] && exit 0 || exit 1;
